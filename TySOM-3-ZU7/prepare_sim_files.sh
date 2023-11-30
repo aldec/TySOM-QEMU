77f4e1565a0982397e6139e8bc6a3c645fb40eeb
Zbyszek Tuchewicz <z.tuchewicz@alltech.gda.pl>
2023-11-30 10:19:35 +0100

    waveform improvement

--- TySOM-3-ZU7/prepare_sim_files.sh
+++ TySOM-3-ZU7/prepare_sim_files.sh
@@ -8,6 +8,8 @@ if [ ! -d riviera ]; then
     mkdir -p riviera
 fi
 
+cp -f src/make_wv.do riviera
+cp -f src/virt_stat.xml riviera
 cp -f hardware/${hw_project_name}/${hw_project_name}.sim/sim_1/behav/riviera/${hw_design_name}_wrapper_compile.do riviera
 cp -f hardware/${hw_project_name}/${hw_project_name}.sim/sim_1/behav/riviera/${hw_design_name}_wrapper_simulate.do riviera
 cp -f src/run.sh riviera
@@ -27,11 +29,14 @@ set QUANTUM_GENERIC_PATH /$env(hw_design_name)_wrapper/$env(hw_design_name)_i/zy
 echo \$QUANTUM_GENERIC_PATH \
 set SIM_OPT \"-ieee_nowarn -i 5000 -t 1ps -relax +notimingchecks +access +r \":' ./riviera/${hw_design_name}_wrapper_simulate.do
 sed -i s'/asim +access +r/eval asim $SIM_OPT/' ./riviera/${hw_design_name}_wrapper_simulate.do
+sed -i s'/+m+design_1_wrapper/+m+design_1_wrapper+axi_monitor/' ./riviera/${hw_design_name}_wrapper_simulate.do
 sed -i '17,$d' ./riviera/${hw_design_name}_wrapper_simulate.do
-sed -i s'/xil_defaultlib.glbl/xil_defaultlib.glbl \\\
+sed -i s'/xil_defaultlib.glbl/xil_defaultlib.glbl work.axi_monitor \\\
 -g$GENERIC_PATH=$env(RPORT_PATH_HDL) -g$QUANTUM_GENERIC_PATH=$env(SYNC_QUANTUM) \n \
-wave \/$env(hw_design_name)_wrapper\/PL_leds* \n \
-wave \/$env(hw_design_name)_wrapper\/$env(hw_design_name)_i\/axi_gpio_1\/* \n \
+do make_wv.do \n \
 run -all/' ./riviera/${hw_design_name}_wrapper_simulate.do
 
 python3 ./scripts/generate_sim_files.py  hardware/${hw_project_name}/${hw_project_name}.gen/sources_1/bd/${hw_design_name}/ip/${hw_design_name}_zynq_ultra_ps_e_0_0/${hw_design_name}_zynq_ultra_ps_e_0_0_stub.vhdl riviera/
+
+sed -i 's:vcom -work xil_defaultlib -2008 ./../src/design_1_zynq_ultra_ps_e_0_0.vhd:vcom -work xil_defaultlib -2008 ./../src/design_1_zynq_ultra_ps_e_0_0.vhd \
+vlog -sv -incr -l xilinx_vip -work work "../src/testbench.sv":' ./riviera/zynq_ultra_compile_cosim.do
